c2a893868651a4daa0ffdaf224ca21a52f9222db
JCelayaRdz <jcelayardz@gmail.com>
2026-02-25 10:04:00 +0100

    add sys stdout logger

---
+++ log.py
@@ -0,0 +1,17 @@
+import logging
+import sys
+
+def setup_logger(name) -> logging.Logger:
+    formatter = logging.Formatter(
+        fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+
+    return logger
