ec06bd7afc8a121b54739a5a5bf5809eef73c9e0
JCelayaRdz <jcelayardz@gmail.com>
2026-02-25 10:04:10 +0100

    add llm evaluator

---
+++ llm_evaluator.py
@@ -0,0 +1,29 @@
+import asyncio
+from openai import AsyncOpenAI
+from tenacity import (
+    retry, 
+    stop_after_attempt, 
+    wait_exponential
+)
+from schemas import EvaluationSchema
+
+class LLMEvaluator:
+    def __init__(self, 
+                 api_key: str, 
+                 gpt_model: str, 
+                 system_prompt: str) -> None:
+        self.client = AsyncOpenAI(api_key=api_key)
+        self.gpt_model = gpt_model
+        self.system_prompt = system_prompt
+
+    @retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=4, max=10))
+    async def evaluate(self, ticket: str, reply: str) -> EvaluationSchema:
+        response = await self.client.beta.chat.completions.parse(
+            model=self.gpt_model,
+            messages=[
+                {"role": "system", "content": self.system_prompt},
+                {"role": "user", "content": f"Ticket {ticket}\nReply: {reply}"}
+            ],
+            response_format=EvaluationSchema
+        )
+        return response.choices[0].message.parsed
