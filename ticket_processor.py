6c32250cf9ef0a5f1221a4b376168dea17c52bbf
JCelayaRdz <jcelayardz@gmail.com>
2026-02-25 10:27:17 +0100

    updated ticket processor

--- ticket_processor.py
+++ ticket_processor.py
@@ -13,19 +13,23 @@ class TicketProcessor:
         self.delimiter = delimiter
         self.df = None
 
-    def load_tickets(self) -> None:
+    def load_tickets(self) -> list[dict]:
         try:
             logger.info(f"Trying to read CSV at path: {self.input_path}")
             self.df = pd.read_csv(self.input_path, delimiter=self.delimiter)
             logger.info("CSV read successfully")
+            return self.df.to_dict(orient="records") 
         except Exception as e:
             logger.error(f"Unable to load CSV at path: {self.input_path}. Message: {str(e)}")
             raise RuntimeError("Unable to read CSV") from e
     
-    def save_tickets(self, evaluated_df: pd.DataFrame) -> None:
+    def save_results(self, results: list[dict]) -> None:
         try:
             logger.info(f"Trying to save CSV at path: {self.output_path}")
-            evaluated_df.to_csv(self.output_path)
+            evaluated_df = pd.DataFrame(results)
+            cols = ["ticket", "reply", "content_score", "content_explanation", "format_score", "format_explanation"]
+            evaluated_df = evaluated_df[cols]
+            evaluated_df.to_csv(self.output_path, index=False) 
             logger.info("CSV file saved successfully")
         except Exception as e:
             logger.error(f"Unable to save CSV at path: {self.output_path}. Message: {str(e)}")
